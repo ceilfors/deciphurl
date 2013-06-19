cf1d5f14bb4e854c13c33d812180c0f66e94544b
Wisen <wisen.tanasa@gmail.com>
2013-06-19 23:27:00 +0800

    Updated config doc

--- src/dist/conf/config.groovy
+++ src/dist/conf/config.groovy
@@ -1,3 +1,7 @@
+/**
+ * This file contains the rules that will be used for deciphurling.
+ * For more information here: https://github.com/ceilfors/deciphurl
+ */
 rule {
     google_define {
         pattern = /([\W\w]+)/
